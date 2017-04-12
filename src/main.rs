9aefc4373d1b939b411dba87b786e5919626eb33
mistrpopo <fpegeot@gmail.com>
2017-04-12 14:13:58 +0900

    URL shortener repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate harsh;
+
+use std::sync::RwLock;
+use rocket::State;
+
+mod repo;
+mod shortener;
+use repo::Repository;
+
+#[get("/<id>")]
+fn lookup(rep: State<RwLock<Repository>>, id: &str) -> String {
+    match rep.read().unwrap().lookup(id) {
+        Some(url) => format!("id {} redirects to {}", id, url),
+        _ => format!("id {} was not found", id)
+    }
+}
+
+#[get("/<url>")]
+fn store(rep: State<RwLock<Repository>>, url: &str) -> String {
+    let mut rep = rep.write().unwrap();
+    rep.store(url);
+    let id = rep.store(&url);
+   format!("url {} was stored with id {}", url, id)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().manage(RwLock::new(Repository::new()))
+                    .mount("/id", routes![lookup])
+                    .mount("/store", routes![store])
+                    .launch();
 }
